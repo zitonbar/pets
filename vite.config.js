07aea5bb67df7d50e7f35882e193b6d748d76f9e
ztionbar <ztion1990@gmail.com>
2024-02-22 22:51:34 +0800

    修改vite.config.js Repository 的名稱

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/pets/',
+  base: '/pets-final/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
